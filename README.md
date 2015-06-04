e02cefff3eb4b5cab9b774860a475b002fa36030
Stuart P. Bentley <stuart@testtrack4.com>
2015-06-04 10:59:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# freshicon
+
+Drop-in script to bust favicon caches on an as-needed basis
+
+## Dependencies
+
+- A server that serves ETag or Last-Modified headers for files in response to
+  HEAD requests.
+- A server that can handle a query string like '?_now=1433441550731' at the end
+  of a request for a static file.
+- localStorage and XMLHttpRequest support in-browser (either native or
+  [polyfilled][1]).
+
+[1]: https://github.com/inexorabletash/polyfill/blob/master/obsolete/storage.js
+
+## Background
+
+Browsers don't cache icons the way they cache other resources. Instead of
+actually requesting a page's icon, they check if the URL has changed, and if
+it hasn't, they don't bother checking to see if there's a new version.
+
+In some ways, this is a useful behavior (in that it reduces the number of
+unnecessary requests to your server), but in others, it's pretty lousy. It
+makes it so, if you change your site's favicon, you have to change the icon's
+URL if you want old visitors to see the new icon without having to clear their
+cache and restart their browser. That means you have to have ugly query strings
+unnecessarily cluttering up your HTML for no particularly good reason.
+
+What browsers *should* do is make a lightweight HEAD request to determine if
+the icon has changed (using existing caching mechanisms like ETag), and update
+if so.
+
+That is what this script does.
+
+## Configuring
+
+By default, freshicon will check the first-listed `href` for a `link` with
+`"icon"`, `"shortcut icon"`, or `"apple-touch-icon"` (in that order of
+precedence) as its `rel` in the `<head>` (or `/favicon.ico`, if no icons are
+listed) for changes every time the page is loaded. To override this, you may
+add a `data-freshicon-check` attribute to a `<link>` element specifying the
+file you wish to use for checking in your document's `<head>`, optionally
+giving it a minimum duration in seconds to wait between checks.
+
+For example, this HTML will set freshicon to check "/favicon-16x16px.png" for
+changes at most once a day:
+
+```html
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- ...snip... -->
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" data-freshicon-check="86400">
+    <!-- ...snip... -->
+  </head>
+  <body>
+    <!-- ...snip... -->
+    <script src="/freshicon.js"></script>
+  </body>
+</html>
+```
+
+## Caveats
+
+freshicon forces a refresh by changing the URL of all icon links in the event
+that the icon has changed. This means that, the next time the page is loaded
+and the icon hasn't changed, the icon will be requested *again* with the
+original URL. If you need to avoid this double-request, you're better off
+using actual URL changes hard-coded into the HTML.
+
+# [License in Three Lines ![(LITL)](https://litl-license.org/logo.svg)][LITL]
+
+[LITL]: https://litl-license.org
+
+Copyright 2015 Stuart P. Bentley.<br>
+This work may be used freely as long as this notice is included.<br>
+The work is provided "as is" without warranty, express or implied.
---
+++ freshicon.js
@@ -0,0 +1,117 @@
+// freshicon.js Copyright 2015 Stuart P. Bentley.
+// This work may be used freely as long as this notice is included.
+// The work is provided "as is" without warranty, express or implied.
+
+(function(){
+
+var headLinks = (document.head || document.getElementsByTagName('head')[0])
+  .getElementsByTagName('link');
+var isIconRel = /^(shortcut |apple-touch-)?icon$/;
+
+var now = new Date();
+
+var cacheBusterParam = '_now=' + now.getTime();
+function cacheBustedUrl(url) {
+  return url + (~url.indexOf('?') ? '&' : '?') + cacheBusterParam;
+}
+
+function cacheBustAllIcons() {
+  var iconLinkCount = 0;
+
+  // Update href attributes for all icon link elements
+  for (var i = 0; i < headLinks.length; i++) {
+    var link = headLinks[i];
+    if (isIconRel.test(link.rel)) {
+      iconLinkCount++;
+      link.href = cacheBustedUrl(link.href);
+    }
+  }
+
+  // if this page has no icon link elements
+  if (iconLinkCount == 0) {
+    // add a cache-busting link to /favicon.ico
+    var faviconIcoLink = document.createElement('link');
+    faviconIcoLink.rel = 'shortcut icon';
+    faviconIcoLink.href = cacheBustedUrl('/favicon.ico');
+  }
+}
+
+var checkLink = null;
+
+for (var i = headLinks.length; i>=0; i--) {
+  var link = headLinks[i];
+  if (isIconRel.test(link.rel)) {
+
+    // if this is the last <link> in <head> with our sentinel attribute
+    if (link.getAttribute('data-freshicon-check') !== null) {
+
+      // we got a winner, stop searching
+      checkLink = link;
+      break;
+
+    // otherwise we prefer the earliest link with the shortest rel
+    } else if (!checkLink || link.rel.length <= checkLink.rel.length) {
+      checkLink = link;
+    }
+  }
+}
+
+var checkUrl;
+var checkLimit = null;
+
+if (checkLink) {
+  checkUrl = checkLink.href;
+  checkLimit = parseInt(
+    checkLink.getAttribute('data-freshicon-check'), 10) * 1000;
+} else {
+  checkUrl = '/favicon.ico';
+}
+
+var lastCheckKey = 'freshicon last check ' + checkUrl;
+var lastCheck = localStorage.getItem(lastCheckKey);
+lastCheck = lastCheck && new Date(lastCheck);
+var nextCheckKey = 'freshicon next check ' + checkUrl;
+var nextCheck = localStorage.getItem(nextCheckKey);
+nextCheck = nextCheck && new Date(nextCheck);
+
+function checkIcon() {
+  var xhr = new XMLHttpRequest();
+  function updateFaviconState() {
+    var lastETagKey = 'freshicon last etag ' + checkUrl;
+    var lastETag = localStorage.getItem(lastETagKey);
+    var lastModKey = 'freshicon last modified ' + checkUrl;
+    var lastMod = localStorage.getItem(lastModKey);
+
+    var thisETag = xhr.getResponseHeader('ETag');
+    var thisLastMod = xhr.getResponseHeader('Last-Modified');
+    var shouldNextCheck = xhr.getResponseHeader('Expires');
+
+    // If there's no data indicating the icon is unchanged
+    if (!((lastETag && lastETag == thisETag) ||
+      (lastMod && thisLastMod == lastMod))) {
+
+      // Ensure we have the newest icon
+      cacheBustAllIcons();
+    }
+
+    // Save results of this check for next check
+    if (thisETag) localStorage.setItem(lastETagKey, thisETag);
+    if (thisLastMod) localStorage.setItem(lastModKey, thisLastMod);
+    if (shouldNextCheck) localStorage.setItem(nextCheckKey, shouldNextCheck);
+  }
+  xhr.open('HEAD', cacheBustedUrl(checkUrl), true);
+  xhr.onreadystatechange = function(evt) {
+    if (xhr.readyState == 4) return updateFaviconState();
+  };
+  xhr.send();
+}
+
+// If we're past the locally-set or remotely-requested cache check limit
+if (checkLimit ? (lastCheck ? lastCheck + checkLimit <= now : true)
+  : nextCheck ? nextCheck <= now : true) {
+
+  // Check to see if the icon has updated
+  checkIcon();
+}
+
+})();
